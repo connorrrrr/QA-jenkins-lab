293e2f3070172fffb4c97f7a87d893c02dbb9371
Connor <58959991+connorrrrr@users.noreply.github.com>
2025-04-16 15:29:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "d183fbaa-dc7f-44e3-b60f-128124512e5f"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstateconnor_1744813379946"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
